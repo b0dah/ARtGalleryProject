4b09b3f2e185e854cfe4513bc18a8407c04cc48f
b0dah <ries8898@gmail.com>
2020-04-19 14:33:12 +0300

    truly initial commit

---
+++ ARtGallery/ARtGallery/ExplorationViewController.swift
@@ -0,0 +1,39 @@
+//
+//  ExplorationViewController.swift
+//  ARtGallery
+//
+//  Created by Иван Романов on 19.04.2020.
+//  Copyright © 2020 Иван Романов. All rights reserved.
+//
+
+import UIKit
+import ARKit
+
+class ExplorationViewController: UIViewController {
+    
+    @IBOutlet weak var sceneView: ARSCNView!
+    
+    
+    // Fields
+    private let configuration = ARWorldTrackingConfiguration()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // sceneView conf
+        sceneView.showsStatistics = true
+        sceneView.debugOptions = [ARSCNDebugOptions.showWorldOrigin]
+        
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        self.sceneView.session.run(configuration)
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        sceneView.session.pause()
+    }
+}
---
+++ ARtGallery/ARtGallery/MuseumSelectionViewController.swift
@@ -0,0 +1,13 @@
+//
+//  MuseumSelectionViewController.swift
+//  ARtGallery
+//
+//  Created by Иван Романов on 19.04.2020.
+//  Copyright © 2020 Иван Романов. All rights reserved.
+//
+
+import UIKit
+
+class MuseumSelectionViewController: UIViewController {
+    
+}
