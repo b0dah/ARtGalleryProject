f50f3b482a7f797287a37903a918eca4d6cccd39
b0dah <ries8898@gmail.com>
2020-04-30 12:32:55 +0300

    model is in its solid state now

--- Server/AGServer/Gallery/models.py
+++ Server/AGServer/Gallery/models.py
@@ -21,18 +21,25 @@ class Artist(models.Model):
 	
 	def __str__(self):
 		return str(self.id) + '  ' + self.name
+		
+# Painting Style
+class Genre(models.Model):
+	name = models.CharField(max_length = 120, primary_key = True)
 	
+	def __str__(self):
+		return self.name
+
 	
 class Painting(models.Model):
 	id = models.AutoField(primary_key = True)
 	title = models.CharField(max_length = 255)
 	year = models.CharField(max_length = 4)
 	description = models.CharField(max_length = 1000)
-	genre = models.CharField(max_length = 50)
 	imageTitle = models.CharField(max_length = 300)
 	
 	#Foreign Keys:
 	author = models.ForeignKey(Artist, on_delete = models.SET_NULL, null=True)
+	genre = models.ForeignKey(Genre, on_delete = models.SET_NULL, null = True)
 	museum_id = models.ForeignKey(Museum, on_delete=models.CASCADE)
 	
 	def __str__(self):
--- Server/AGServer/Gallery/serializers.py
+++ Server/AGServer/Gallery/serializers.py
@@ -7,18 +7,7 @@ class ArtistSerializer(serializers.ModelSerializer):
 		model = Artist
 		fields = ['id', 'name', 'yearsOfLife', 'country', 'portraitImageTitle']
 
-	
-	
-	
-
 class PaintingSerializer(serializers.ModelSerializer):
-#	id = serializers.IntegerField()
-#	title = serializers.CharField(max_length = 255)
-#	year = serializers.CharField(max_length = 4)
-#	
-#	description = serializers.CharField(max_length = 1000)
-#	genre = serializers.CharField(max_length = 50)
-#	imageTitle = serializers.CharField(max_length = 300)
 		
 	#Foreign Keys:
 	museum_id = serializers.PrimaryKeyRelatedField(queryset=Museum.objects.all())
@@ -34,9 +23,9 @@ class PaintingSerializer(serializers.ModelSerializer):
 		return Paiting.objects.create(**validated_data)
 	
 
-class MuseumSerializer(serializers.Serializer):
-	id = serializers.IntegerField()
-	name = serializers.CharField(max_length = 300)
-	country = serializers.CharField(max_length = 100)
-	city = serializers.CharField(max_length = 100)
-	imageTitle = serializers.CharField(max_length = 300)
+class MuseumSerializer(serializers.ModelSerializer):
+	
+	class Meta:
+		model = Museum 
+		
+		fields = ['id', 'name', 'country', 'city', 'imageTitle']
